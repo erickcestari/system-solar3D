b0016a7875d759745e9e39ae06cb5cf2793ed961
Erick Cestari <erickcestari03@gmail.com>
2023-12-02 14:42:06 -0300

    feat: use threejs to render stars

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
 import './style.css'
+
+import * as THREE from "three"
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
+const canvas = document.querySelector('#bg')
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(30)
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial({ color: 0xff63, wireframe: true })
+const torus = new THREE.Mesh(geometry, material)
+
+scene.add(torus)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(ambientLight)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+
+  star.position.set(x, y, z)
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  controls.update()
+
+  renderer.render(scene, camera);
+}
+
+animate()
